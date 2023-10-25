a2d91a3d2f2752e768cf274e67990ef11e5d44ee
esaupdf <127408399+esaupdf@users.noreply.github.com>
2023-10-24 21:05:30 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
-import Express from 'express';
-import logger from 'morgan';
-import rotasDeUsuarios from './rotas/rotasDeUsuarios.js';
+import rotasDePagamentos from './rotas/rotasDePagamentos.js'
+import rotasDeAtentimentos from './rotas/rotasDeAtendimentos.js'
+import rotasDeConsultasrealizadas from './rotas/rotasDeConsultasrealizadas.js'
+import rotasDeConsultasagendadas from './rotas/rotasDeConsultasagendadas.js'
+import rotasDeChat from './rotas/rotasDeChat.js'
+import rotasDeConfiguraçoes from './rotas/rotasDeConfiguraçoes.js'
 
-const app = Express();
+
+const app = express ();
 app.use(logger('dev'));
 
-app.use('/usuarios', rotasDeUsuarios);
+app.use('pagamentos', rotasDePagamentos);
+app.use('atendimentos', rotasDeAtentimentos);
+app.use('consultasrealizadas', rotasDeConsultasrealizadas);
+app.use('consultasagendadas', rotasDeConsultasagendadas);
+app.use('chat', rotasDeChat);
+app.use('configuraçoes', rotasDeConfiguraçoes);
+
+
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
-});
+  });
